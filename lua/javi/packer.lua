f62f0c4f8fb9cddf01b92b99ce2377d2b572c491
Javier-DlaP <javirachi99@hotmail.es>
2023-01-04 16:24:13 +0100

    Gitsigns added

--- lua/javi/packer.lua
+++ lua/javi/packer.lua
@@ -53,4 +53,6 @@ return require('packer').startup(function(use)
 	  'nvim-lualine/lualine.nvim',
 	  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
 	}
+
+	use 'lewis6991/gitsigns.nvim'
 end)
