66d1b8ad351b38462f9ea8c5b0551394924614d4
Javier-DlaP <javirachi99@hotmail.es>
2023-01-08 20:12:28 +0100

    Auto LSP and Packer setup

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,4 +1,15 @@
 local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
+
+lsp.ensure_installed({
+	'jedi_language_server',
+	'sumneko_lua',
+	'grammarly',
+	'dockerls',
+})
+
+-- Configure lua languaje server for neovim
+lsp.nvim_workspace()
+
 lsp.setup()
