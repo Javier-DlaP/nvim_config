cc902fb7d271e99790e7154d37780e153eec1d5d
Javier_de_la_Peña <javier.delapena@icp.es>
2023-02-03 08:41:58 +0100

    Enable YAML with Copilot

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -2,4 +2,6 @@ vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-j>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 
 -- Enable markdown languaje for copilot
-vim.g.copilot_filetypes = { ["markdown"] = true }
+vim.g.copilot_filetypes = {["markdown"] = true,
+						   ["yaml"] = true,
+					   	  }
