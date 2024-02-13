ffb2bb4afcb178d06e39ad421f9d872967c181d5
Javier de la Peña <javi@MacBook-Pro-de-Javier.local>
2024-02-13 11:28:34 +0100

    Remove lsp servers instalation

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,14 +2,4 @@ local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
 
-lsp.ensure_installed({
-	'jedi_language_server',
-	'sumneko_lua',
-	--'grammarly',
-	--'dockerls',
-})
-
--- Configure lua languaje server for neovim
-lsp.nvim_workspace()
-
 lsp.setup()
